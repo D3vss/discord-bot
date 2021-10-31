c4b4992996989839cb13edda2d94e30303bf8168
D3vss <mohamed_oudoun@um5.ac.ma>
2021-10-31 14:06:19 +0100

    successfully interracting with 'Ping' command

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 require("dotenv").config();
 
+const token = process.env.DISCORD_TOKEN;
+const appID = process.env.APPLICATION_ID;
+
 const Discord = require("discord.js");
 
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 client.once("ready", () => {
-  console.log(`Connected as ${client.user.id}!`);
+  console.log(`Connected as ${client.user.username} !`);
 });
 
-client.login(process.env.DISCORD_TOKEN);
+client.on("messageCreate", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!");
+  }
+});
+
+client.login(token);
